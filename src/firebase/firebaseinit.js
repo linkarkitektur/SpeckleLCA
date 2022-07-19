d8c3c17f7ba792a93a398cc5313390e7fc3162ca
ananthan muthu <ananthan.muthu@maybank.com>
2022-07-19 16:22:28 +0530

    firebase integration and bug fixes

--- src/firebase/firebaseinit.js
+++ src/firebase/firebaseinit.js
@@ -1,18 +1,17 @@
-import firebase from "firebase/app";
-import { serverTimestamp } from "firebase/firestore";
-import "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { getFirestore, doc } from "firebase/firestore";
 
 const config = {
   apiKey: process.env.VUE_APP_FIRE_BASE_API_KEY,
   authDomain: process.env.VUE_APP_FIRE_BASE_URL,
   projectId: "linklca",
-  storageBucket: "linklca.appspot.com",
+  storageBucket: "linklca.appspot.com", 
   messagingSenderId: "672281257450",
   appId: "1:672281257450:web:6fe2b06e8b90f11b063e93",
 };
 
-const firebaseApp = firebase.initializeApp(config);
-const timestamp = serverTimestamp();
+const firebaseApp = initializeApp(config);
+const db = getFirestore(firebaseApp);
 
-export { timestamp };
-export default firebaseApp.firestore();
+const mapperDB = doc(db, "mapperList/linkLca");
+export default mapperDB;
