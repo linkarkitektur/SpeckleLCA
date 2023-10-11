4df5d823a1a837f557ddce8ad8357ad89a5d6f4b
Fabian Sellberg <fabian.sellberg@linkarkitektur.se>
2023-10-11 11:15:33 +0200

    Store setup

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -7,6 +7,6 @@ export const useCounterStore = defineStore('counter', () => {
   function increment() {
     count.value++
   }
-
+  
   return { count, doubleCount, increment }
 })
---
+++ src/stores/main.ts
@@ -0,0 +1,71 @@
+import { defineStore } from "pinia";
+
+export const useProjectStore = defineStore({
+  id: "projectStore",
+  state: () =>
+    {
+      return {
+        currProject: null as Project | null,
+      }
+    },
+
+  actions: {
+    createNewProject(project: Project) {
+      this.currProject = project;
+    },
+
+    updateProject(project: Project) {
+      this.currProject = project;
+    },
+
+    addGeometry(geometryObject: GeometryObject) {
+      this.currProject?.geometry.push(geometryObject);
+    },
+
+    removeGeometry(id: string) {
+      const index = this.findGeometryIndexById(id);
+      
+      if(index === -1) return;
+      
+      if (index != undefined)
+        this.currProject?.geometry.splice(index, 1);
+    },
+
+    updateGeometry(payload: GeometryObject, id: string) {
+      if (!id || !payload || this.currProject === null) return;
+
+      const index = this.findGeometryIndexById(id);
+
+      if (index !== -1 && index != undefined)
+        this.currProject.geometry[index] = payload;
+    },
+
+    addResults(result: Results) {
+      if(this.currProject?.results != null)
+        this.currProject.results.push(result);
+      else if(this.currProject != null)
+        this.currProject.results = [result];
+    },
+
+    updateResults(payload: Results, id: string) {
+      if (!id || !payload || this.currProject === null) return;
+
+      const index = this.findResultIndexById(id);
+
+      if (index !== -1 && index != undefined && this.currProject.results != null)
+        this.currProject.results[index] = payload;
+      else if (this.currProject != null)
+        this.currProject.results[index] = payload;
+    },
+
+    findGeometryIndexById(id: string) {
+      return this.currProject?.geometry.findIndex((item) => item.id === id);
+    },
+
+    findResultIndexById(id: string) {
+      return this.currProject?.geometry.findIndex((item) => item.id === id);
+    },
+
+
+  },
+});
