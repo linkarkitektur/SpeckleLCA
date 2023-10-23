10b3aac06a89d71dc5ee630610f60e212a205101
Ryan Hughes <whoisryan@pm.me>
2023-10-23 17:38:04 +0200

    Commented tests out. Updated tsconfig.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,58 +1,59 @@
 <template>
-    <v-container fluid fill-height class="home flex-column justify-center align-center primary--text">
-        <h1>Hi {{ user.name }}!!</h1>
-        <p>
-            Search for a stream in the navigation bar, or pick from one of your latest
-            👇🏼
-        </p>
-        <v-list v-if="streams" max-height="900px" class="overflow-y-auto">
-            <v-list-item-group>
-                <v-list-item v-for="stream in streams.items" :key="stream.id" @click="navigateToStream(stream.id)">
-                    <v-list-item-content>
-                        <v-list-item-title>
-                            <v-row class="pa-0 ma-0">
-                                {{ stream.name }}
-                                <v-spacer></v-spacer>
-                                <span class="primary rounded white--text pl-1 pr-1 caption">{{ stream.id }}</span>
-                            </v-row>
-                        </v-list-item-title>
-                        <v-list-item-subtitle class="caption primary--text">
-                            Updated
-                            <timeago :datetime="stream.updatedAt"></timeago>
-                        </v-list-item-subtitle>
-                    </v-list-item-content>
-                </v-list-item>
-            </v-list-item-group>
-        </v-list>
-    </v-container>
+  <v-container fluid fill-height class="home flex-column justify-center align-center primary--text">
+    <h1>Hi {{ user.name }}!!</h1>
+    <p>Search for a stream in the navigation bar, or pick from one of your latest 👇🏼</p>
+    <v-list v-if="streams" max-height="900px" class="overflow-y-auto">
+      <v-list-item-group>
+        <v-list-item
+          v-for="stream in streams.items"
+          :key="stream.id"
+          @click="navigateToStream(stream.id)"
+        >
+          <v-list-item-content>
+            <v-list-item-title>
+              <v-row class="pa-0 ma-0">
+                {{ stream.name }}
+                <v-spacer></v-spacer>
+                <span class="primary rounded white--text pl-1 pr-1 caption">{{ stream.id }}</span>
+              </v-row>
+            </v-list-item-title>
+            <v-list-item-subtitle class="caption primary--text">
+              Updated
+              <timeago :datetime="stream.updatedAt"></timeago>
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-container>
 </template>
 
 <script>
-import { ref, onMounted } from 'vue';
-import { useSpeckleStore } from '@/store/speckle';
-import { getStreams } from '@/utils/speckleUtils';
+import { ref, onMounted } from 'vue'
+import { useSpeckleStore } from '@/store/speckle'
+import { getStreams } from '@/utils/SpeckleUtils'
 
 export default {
-    name: "Home",
+  name: 'Home',
 
-    setup() {
-        const store = useSpeckleStore();
+  setup() {
+    const store = useSpeckleStore()
 
-        // Fetch streams when the component is mounted
-        onMounted(async () => {
-            store.fetchStreams();
-        });
+    // Fetch streams when the component is mounted
+    onMounted(async () => {
+      store.fetchStreams()
+    })
 
-        const navigateToStream = (streamId) => {
-            // Use Vue Router to navigate to the stream
-            router.push(`/streams/${streamId}`);
-        };
+    const navigateToStream = (streamId) => {
+      // Use Vue Router to navigate to the stream
+      router.push(`/streams/${streamId}`)
+    }
 
-        return {
-            user: store.user,
-            streams: store.streams,
-            navigateToStream,
-        };
-    },
-};
-</script>@/utils/SpeckleUtils
+    return {
+      user: store.user,
+      streams: store.streams,
+      navigateToStream
+    }
+  }
+}
+</script>@/utils/SpeckleUtils.js
