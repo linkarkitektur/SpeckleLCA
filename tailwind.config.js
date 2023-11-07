ee8424f56de87906f5d26e1e8fd1c11139e99ca7
fabianlinkflink <fabian.sellberg@linkarkitektur.se>
2023-11-07 16:52:01 +0100

    Headlessui and heroicons added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
 
