83d14915bc102b9c30eac417aad10099eb083abc
fabianlinkflink <fabian.sellberg@linkarkitektur.se>
2023-11-14 18:32:01 +0100

    vuetify removed

--- src/main.ts
+++ src/main.ts
@@ -7,17 +7,6 @@ import App from './App.vue'
 import router from './router'
 import './index.css'
 
-// Vuetify
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-
-const vuetify = createVuetify({
-    components,
-    directives,
-})
-
 const app = createApp(App)
 
 Sentry.init({
@@ -52,7 +41,6 @@ if (Sentry.getCurrentHub().getClient()) {
 }
 
 app.use(createPinia())
-app.use(vuetify)
 app.use(router)
 
 app.mount('#app')
