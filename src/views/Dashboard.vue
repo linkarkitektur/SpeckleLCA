aa504c1d04dc4a9eada94d82aa12d5aa08079b20
fabianlinkflink <fabian.sellberg@linkarkitektur.se>
2023-11-21 22:48:50 +0100

    Main views updated

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,22 @@
+<template>
+    <Navbar />
+    <Sidebar />
+</template>
+
+
+<script lang="ts">
+import { defineComponent, ref, computed } from 'vue';
+import Sidebar from '@/components/Sidebar/Sidebar.vue';
+import Navbar from '@/components/Navbar.vue';
+import '@/index.css';
+
+export default defineComponent({
+  name: "Dashboard",
+  components: {
+    Navbar,
+    Sidebar
+  },
+  setup() {
+  },
+});
+</script>
