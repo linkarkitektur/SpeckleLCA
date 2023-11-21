e916ea6cbb8db6c2ddacc9c24a787609e8f23332
fabianlinkflink <fabian.sellberg@linkarkitektur.se>
2023-11-21 22:50:25 +0100

    Update SpeckleViewer.vue

---
+++ src/components/ModelViewer/SpeckleViewer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex-1 p-6 bg-gray-100">
+    <h1 class="text-2xl font-bold mb-4">Model View</h1>
+    <!-- Your content for the ModelView -->
+    <div class="bg-white p-4 shadow-md rounded-md">
+      <!-- Example content -->
+      <p>Your model content goes here...</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent ({
+  name: "SpeckleViewer",
+  components: {},
+  setup() {
+
+  },
+  // Component logic
+});
+</script>
