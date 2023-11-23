2692ba378354bf64ac21d8e34b81bba1e629bda8
fabianlinkflink <fabian.sellberg@linkarkitektur.se>
2023-11-23 17:00:17 +0100

    Update speckle.ts

--- src/stores/speckle.ts
+++ src/stores/speckle.ts
@@ -154,6 +154,54 @@ export const useSpeckleStore = defineStore({
         console.error(err);
       }
     },
+    
+    /**
+     * The `updateProjectVersions` action updates the project versions for the specified project.
+     * @param {string} projectId - The ID of the project to get versions for.
+     * @param {number} limit - The maximum number of versions to get.
+     * @param {Date | null} cursor - The cursor to use for pagination.
+     * @returns {Promise<void>}
+     */
+    async updateProjectVersions(projectId: string, limit: number, cursor: Date | null) {
+      try {
+        const response = await getProjectVersions(projectId, limit, cursor);
+        const projDet: ProjectDetails = await response.json().data;
+        let allVers: VersionId[];
+        allVers = (projDet.versions?.items || []).map((el, key) => {
+          return { id: el.id, name: el?.message ?? "Version " + key };
+        });
+
+        let allModels: string[];
+        let modelVers: ModelsAndVersions = {};
+
+        if (
+          projDet.versions?.items &&
+          projDet.versions?.items.length > 0) {
+          projDet.versions?.items?.forEach((version) => {
+            const { modelName } = version;
+            if (!modelVers[modelName]) {
+              modelVers[modelName] = [];
+            }
+            modelVers[modelName].push(version);
+          });
+
+          allModels = projDet.versions?.items?.map(function (version) {
+            return version?.modelName;
+          });
+        }
+
+        // Use this.$patch instead of commit to update state
+        this.$patch((state) => {
+          state.projectDetails = projDet;
+          state.selectedVersion = projDet.versions?.items?.[0];
+          state.allVersions = allVers;
+          state.allModels = allModels;
+          state.modelsAndVersions = modelVers;
+        });
+      } catch (err) {
+        console.error(err);
+      }
+    },
 
     /**
      * The `setProjectDetails` action sets the project details for the currently selected project.
@@ -234,54 +282,6 @@ export const useSpeckleStore = defineStore({
     removeCustomParameter(name: string) {
       if(this.customParameters)
         this.customParameters = this.customParameters?.filter(item => item.name !== name);
-    },
-
-    /**
-     * The `getStreamAction` action gets the project versions for the specified project.
-     * @param {string} projectId - The ID of the project to get versions for.
-     * @param {number} limit - The maximum number of versions to get.
-     * @param {Date | null} cursor - The cursor to use for pagination.
-     * @returns {Promise<void>}
-     */
-    async getStreamAction(projectId: string, limit: number, cursor: Date | null) {
-      try {
-        const response = await getProjectVersions(projectId, limit, cursor);
-        const projDet: ProjectDetails = await response.json().data;
-        let allVers: VersionId[];
-        allVers = (projDet.versions?.items || []).map((el, key) => {
-          return { id: el.id, name: el?.message ?? "Version " + key };
-        });
-
-        let allModels: string[];
-        let modelVers: ModelsAndVersions = {};
-
-        if (
-          projDet.versions?.items &&
-          projDet.versions?.items.length > 0) {
-          projDet.versions?.items?.forEach((version) => {
-            const { modelName } = version;
-            if (!modelVers[modelName]) {
-              modelVers[modelName] = [];
-            }
-            modelVers[modelName].push(version);
-          });
-
-          allModels = projDet.versions?.items?.map(function (version) {
-            return version?.modelName;
-          });
-        }
-
-        // Use this.$patch instead of commit to update state
-        this.$patch((state) => {
-          state.projectDetails = projDet;
-          state.selectedVersion = projDet.versions?.items?.[0];
-          state.allVersions = allVers;
-          state.allModels = allModels;
-          state.modelsAndVersions = modelVers;
-        });
-      } catch (err) {
-        console.error(err);
-      }
     },
   },
   getters: {
