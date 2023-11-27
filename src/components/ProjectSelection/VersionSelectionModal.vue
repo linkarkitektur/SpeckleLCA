ca170aa51a369aab4f147afec5d03b505143111d
fabianlinkflink <fabian.sellberg@linkarkitektur.se>
2023-11-27 08:54:19 +0100

    Update VersionSelectionModal.vue

--- src/components/ProjectSelection/VersionSelectionModal.vue
+++ src/components/ProjectSelection/VersionSelectionModal.vue
@@ -8,13 +8,13 @@
       <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
         <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
           <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200" leave-from="opacity-100 translate-y-0 sm:scale-100" leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
-            <DialogPanel class="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
+            <DialogPanel class="relative transform rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
               <div>
                 <div class="mx-auto flex h-12 w-auto items-center justify-center rounded-md bg-green-100">
                   <p>{{ projectName }}</p>
                 </div>
                 <div class="mt-3 text-center sm:mt-5">
-                  <Dropdown :items="extractNames" name="Version"/>
+                  <Dropdown :items="extractNames" @selectedItem="handleSelectedItem" name="Version"/>
                   <!--<DialogTitle as="h3" class="text-base font-semibold leading-6 text-gray-900">Here you will select your current version</DialogTitle>
                   <div class="mt-2">
                     <p class="text-sm text-gray-500">{{ projectId }}</p>
@@ -25,7 +25,7 @@
               <div class="mt-5 sm:mt-6">
                 <button type="button" 
                   class="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" 
-                  @click="loadVersion">
+                  @click="loadProject">
                   Load this version
                 </button>
               </div>
@@ -39,15 +39,15 @@
 
 <script lang="ts">
 import { defineComponent, ref, watch, getCurrentInstance, computed } from 'vue'
-import { Dialog, DialogPanel, DialogTitle, TransitionChild, TransitionRoot } from '@headlessui/vue'
+import { Dialog, DialogPanel, DialogTitle, MenuItem, TransitionChild, TransitionRoot } from '@headlessui/vue'
 import { CheckIcon } from '@heroicons/vue/24/outline'
-import Dropdown from '@/components/Dropdown.vue'
+import Dropdown, { type dropdownItem } from '@/components/Dropdown.vue'
 
 import router from '@/router';
-import type { VersionId } from '@/models/speckle';
 import { useNavigationStore, useProjectStore } from '@/stores/main';
 import { useSpeckleStore } from '@/stores/speckle';
 import { getProjectVersions } from '@/utils/speckleUtils';
+import type { ProjectDetails, Version } from '@/models/speckle';
 
 export default defineComponent ({
   name: "VersionSelectionModal",
@@ -74,18 +74,6 @@ export default defineComponent ({
       required: true,
     }
   },
-  methods: {
-    loadVersion() {
-      //Do conversion from speckle project to projectStore project here
-      console.log("Loading version");
-
-      const navigationStore = useNavigationStore();
-
-      navigationStore.setActivePage("Overview");
-      
-      router.push('/dashboard');
-    }
-  },
   setup(props) {
     const isOpen = ref(props.show);
     const projectId = ref(props.projectId);
@@ -93,25 +81,76 @@ export default defineComponent ({
 
     const instance = getCurrentInstance();
     const speckleStore = useSpeckleStore();
+    const navigationStore = useNavigationStore();
 
+    /**
+     * Close the modal and emitting back that it was closed
+     */
     const closeModal = () => {
       isOpen.value = false;
       // Emit back to parent so it knows the modal has been closed and can open a new one
       instance?.emit('closeVersionModal');
     };
 
-    // Return the extracted names from all available versions to be used in dropdown
+    /**
+     *  Return the extracted names from all available versions of the project to be used in dropdown
+     */
     const extractNames = computed(() => {
-      let versions: string[] = []
+      let versions: dropdownItem[] = [];
 
-      speckleStore.getAllVersions?.forEach(item => {
-        if (typeof item.name === 'string') {
-          versions.push(item.name);
+      speckleStore.getAllVersions?.forEach(el => {
+        if (typeof el.message === 'string') {
+          
+          const item: dropdownItem = {
+            name: el.message,
+            data: el.id,
+          }
+
+          versions.push(item);
         }
       });
       return versions;
     });
 
+    /**
+     * Sets the selected version from dropdown selected
+     * @param selectedItem 
+     */
+    const handleSelectedItem = (selectedItem: dropdownItem) => {
+      const version = speckleStore.getAllVersions?.find(obj => obj.id === selectedItem.data)
+      
+      if (version)
+        speckleStore.setSelectedVersion(version);
+    };
+
+    /**
+     * Load version into the project store and navigating to a project view
+     */
+    const loadProject = async () => {
+      //Do conversion from speckle project to projectStore project here
+      console.log("Loading version");
+      let version: Version;
+      if (speckleStore.getProjectDetails) {
+        const versionFound = speckleStore.getProjectDetails.stream.commits.items.find(obj => obj.id === speckleStore.getSelectedVersion?.id);
+
+        if (versionFound) {
+          version = versionFound;
+          speckleStore.setSelectedVersion(version);
+        }
+        else {
+          // TODO: Throw error message here to user
+          console.error("Couldnt find the selected versions");
+        }
+      }
+
+      const objects = await speckleStore.getObjects()
+
+      navigationStore.setActivePage("Overview");
+      
+      router.push('/dashboard');
+    };
+
+    // Properties watched
     watch(() => props.show, (newValue) => {
       isOpen.value = newValue;
     });
@@ -131,6 +170,8 @@ export default defineComponent ({
       projectName,
       speckleStore,
       extractNames,
+      handleSelectedItem,
+      loadProject,
       closeModal,
     }
   }
