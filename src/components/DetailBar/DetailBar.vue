ca7d8d5c7e635ce18713766a4162e6bdb30837e8
fabianlinkflink <fabian.sellberg@linkarkitektur.se>
2024-01-11 17:45:45 +0100

    Added detailbar

---
+++ src/components/DetailBar/DetailBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="DetailBar" class="absolute flex bottom-0 z-10 left-1/2 -translate-x-1/2 w-5/6 h-24 mb-8 rounded-2xl bg-gray-500 bg-opacity-50">
+    <div class="flex w-full items-center justify-center">
+      <label class="text-center">Detail bar info <br> Additional dynamic info goes here </label> 
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'DetailBar',
+  components: {},
+  setup() {
+    return {}
+  },
+})
+</script>
