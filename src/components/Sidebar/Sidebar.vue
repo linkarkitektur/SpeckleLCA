b0e421c33a4206d65d00b8bc36942970f27062fe
fabianlinkflink <fabian.sellberg@linkarkitektur.se>
2024-04-05 16:33:12 +0200

    Collapsable sidebar

--- src/components/Sidebar/Sidebar.vue
+++ src/components/Sidebar/Sidebar.vue
@@ -3,21 +3,63 @@
   <NewGroupModal />
   <MaterialMappingModal />
   <!-- End of modal area -->
-
-  <div class="relative mt-16 z-40 flex h-[calc(100vh-4rem)] w-1/3 flex-col">
+  <button
+    v-if="!sideBarShow"
+    @click="toggleSideBar" 
+    class="absolute top-1/2 pt-16 left-2 transform -translate-y-1/2 mr-2 z-50"
+  >
+    <ChevronRightIcon class="w-6 h-6 opacity-50" />
+  </button>
+  <TransitionRoot as="template" :show="sideBarShow">
     <div 
-      class="flex grow h-[calc(100vh-12rem)] flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6 pt-4"
+      class="relative mt-16 z-40 flex h-[calc(100vh-4rem)] w-1/2 flex-col"
     >
-      <GroupList />
+      <TransitionChild
+        as="template"
+        enter="transform transition ease-in-out duration-500 sm:duration-700"
+        enter-from="-translate-x-full"
+        enter-to="translate-x-0"
+        leave="transform transition ease-in-out duration-500 sm:duration-700"
+        leave-from="translate-x-0"
+        leave-to="-translate-x-full"
+      >
+        <div 
+          class="flex grow h-[calc(100vh-12rem)] flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6 pt-4"
+        >
+          <button
+            v-if="sideBarShow"
+            @click="toggleSideBar" 
+            class="absolute top-1/2 -right-8 transform -translate-y-1/2 mr-2 opacity-50"
+          >
+            <ChevronLeftIcon class="w-6 h-6" />
+          </button>
+          <GroupList />
+        </div>
+      </TransitionChild>
     </div>
-  </div>
+  </TransitionRoot>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import {  
+  TransitionChild, 
+  TransitionRoot 
+} from '@headlessui/vue'
+
+import { 
+  ChevronRightIcon, 
+  ChevronLeftIcon 
+} from '@heroicons/vue/24/solid'
+
+import { useNavigationStore } from '@/stores/main'
+import { useSpeckleStore } from '@/stores/speckle'
+
 import GroupList from '@/components/Sidebar/GroupList.vue'
 import NewGroupModal from '@/components/Sidebar/NewGroupModal.vue'
 import MaterialMappingModal from '@/components/Mapping/MaterialMappingModal.vue'
+
+import { storeToRefs } from 'pinia'
 
 export default defineComponent({
   name: 'SideBar',
@@ -25,9 +67,24 @@ export default defineComponent({
     GroupList,
     NewGroupModal,
     MaterialMappingModal,
+    TransitionChild, 
+    TransitionRoot,
+    ChevronLeftIcon,
+    ChevronRightIcon
   },
   setup() {
-    return {}
+    const navStore = useNavigationStore()
+    const { sideBarShow } = storeToRefs(navStore)
+
+    const toggleSideBar = () => {
+      navStore.toggleSideBar()
+    }
+
+    
+    return {
+      sideBarShow,
+      toggleSideBar
+    }
   }
   
 })
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,11 +2,6 @@
 	<div class="flex">
 		<NavbarComponent />
 		<Sidebar />
-		<SpeckleStats
-			:visible="statsVisible"
-			:names="statNames"
-			:vals="statValues"
-		/>
 		<Suspense>
 			<SpeckleViewer />
 		</Suspense>
@@ -17,8 +12,6 @@
 <script lang="ts">
 	import Sidebar from '@/components/Sidebar/Sidebar.vue'
 	import Slideover from '@/components/SlideOver/Sliderover.vue'
-	import type { ViewerStats } from '@/models/speckle'
-	import SpeckleStats from '@/components/ModelViewer/SpeckleStats.vue'
 	import SpeckleViewer from '@/components/ModelViewer/SpeckleViewer.vue'
 	import NavbarComponent from '@/components/Navbar.vue'
 	import { useMaterialStore } from '@/stores/material'
@@ -31,7 +24,6 @@
 		name: 'DashboardView',
 		components: {
 			NavbarComponent,
-			SpeckleStats,
 			SpeckleViewer,
 			Sidebar,
 			Slideover,
@@ -60,30 +52,12 @@
 				required: false
 			}
 		},
-		setup(props) {
-			const materialStore = useMaterialStore()
+		setup() {
 			//Load materials from the store on startup
+			const materialStore = useMaterialStore()
     	materialStore.materialsFromJson()
-			let statNames: Array<string>
-			let statValues: Array<object>
 
-			if (props.statsVisible) {
-				// If props are not provided, use these defaults, otherwise deconstuct the given stats object.
-				const stats = (props.stats as ViewerStats)
-					? props.stats
-					: { names: null, vals: null }
-
-				statNames = stats.names ? stats.names : ['Elements', ' Stream']
-				statValues = stats.vals ? stats.vals : [18, ""]
-			} else {
-				statNames = []
-				statValues = []
-			}
-
-			return {
-				statNames,
-				statValues
-			}
+			return {}
 		}
 	}
 </script>
