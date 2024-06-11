cdff7e98dff1ff626541a6d949a67060dbf07bbf
fabianlinkflink <fabian.sellberg@linkarkitektur.se>
2024-06-11 23:33:32 +0200

    Added  dynamic graphs to viewer

--- src/components/ModelViewer/ViewerControls.vue
+++ src/components/ModelViewer/ViewerControls.vue
@@ -1,48 +1,45 @@
 <template>
-  <div
-    class="absolute h-1/3 mx-auto top-4 right-4 align-right justify-center z-20"
-  >
-    <span
-      class="isolate inline-flex flex-col rounded-lg shadow-md shadow-gray-300 text-gray-400"
-    >
-      <button
-        type="button"
-        @click="handleProjectionButtonClick"
-        class="inline-flex items-center rounded-l-md bg-white px-3 py-2 ring-1 ring-inset ring-gray-200 hover:bg-gray-200 focus:z-10"
-      >
-        <VideoCameraIcon class="h-6" />
-      </button>
 
-      <button
-        type="button"
-        @click="handleSelectAllButton"
-        class="-ml-px inline-flex items-center bg-white px-3 py-2 ring-1 ring-inset ring-gray-200 hover:bg-gray-200 focus:z-10"
-      >
-        <ViewfinderCircleIcon class="h-6" />
-      </button>
-      <button
-        type="button"
-        @click="handleZoomExtentsButtonClick"
-        class="-ml-px inline-flex items-center rounded-r-md bg-white px-3 py-2 ring-1 ring-inset ring-gray-200 hover:bg-gray-200 focus:z-10"
-      >
-        <ArrowsPointingOutIcon class="h-6" />
-      </button>
-      <button
-        type="button"
-        @click="handleToggleSectionBoxButtonClick"
-        class="-ml-px inline-flex items-center rounded-r-md bg-white px-3 py-2 ring-1 ring-inset ring-gray-200 hover:bg-gray-200 focus:z-10"
-      >
-        <CubeTransparentIcon class="h-6" />
-      </button>
-      <button
-        type="button"
-        @click="handleLockOrbitButtonClick"
-        class="-ml-px inline-flex items-center rounded-r-md bg-white px-3 py-2 ring-1 ring-inset ring-gray-200 hover:bg-gray-200 focus:z-10"
-      >
-        <LockClosedIcon class="h-6" />
-      </button>
-    </span>
-  </div>
+  <span
+    class="isolate inline-flex flex-col rounded-lg shadow-md shadow-gray-300 text-gray-400"
+  >
+    <button
+      type="button"
+      @click="handleProjectionButtonClick"
+      class="inline-flex items-center rounded-l-md bg-white px-3 py-2 ring-1 ring-inset ring-gray-200 hover:bg-gray-200 focus:z-10"
+    >
+      <VideoCameraIcon class="h-6" />
+    </button>
+
+    <button
+      type="button"
+      @click="handleSelectAllButton"
+      class="-ml-px inline-flex items-center bg-white px-3 py-2 ring-1 ring-inset ring-gray-200 hover:bg-gray-200 focus:z-10"
+    >
+      <ViewfinderCircleIcon class="h-6" />
+    </button>
+    <button
+      type="button"
+      @click="handleZoomExtentsButtonClick"
+      class="-ml-px inline-flex items-center rounded-r-md bg-white px-3 py-2 ring-1 ring-inset ring-gray-200 hover:bg-gray-200 focus:z-10"
+    >
+      <ArrowsPointingOutIcon class="h-6" />
+    </button>
+    <button
+      type="button"
+      @click="handleToggleSectionBoxButtonClick"
+      class="-ml-px inline-flex items-center rounded-r-md bg-white px-3 py-2 ring-1 ring-inset ring-gray-200 hover:bg-gray-200 focus:z-10"
+    >
+      <CubeTransparentIcon class="h-6" />
+    </button>
+    <button
+      type="button"
+      @click="handleLockOrbitButtonClick"
+      class="-ml-px inline-flex items-center rounded-r-md bg-white px-3 py-2 ring-1 ring-inset ring-gray-200 hover:bg-gray-200 focus:z-10"
+    >
+      <LockClosedIcon class="h-6" />
+    </button>
+  </span>
 </template>
 
 <script setup lang="ts">
