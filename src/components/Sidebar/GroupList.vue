0b8eaf75ff01d2738ca3f87c50bf2fc4bbd1a79a
fabianlinkflink <fabian.sellberg@linkarkitektur.se>
2024-06-30 23:47:41 +0200

    GroupList cleanup

--- src/components/Sidebar/GroupList.vue
+++ src/components/Sidebar/GroupList.vue
@@ -1,6 +1,10 @@
 <template>
 	<nav class="flex flex-1 flex-col pt-4">
 		<div class="relative h-12">
+			<Dropdown
+				:items="dropdownItems"
+				@selectedItem="handleSelected"
+			/>
 			<button
 				aria-label="Expand"
 				class="absolute flex top-0 right-0 p-1 focus:outline-none focus:shadow-outline text-gray-700 hover:text-gray-800"
@@ -33,119 +37,218 @@
 </template>
 
 <script lang="ts">
-	import { defineComponent, ref, onMounted, watch, computed } from 'vue'
-	import Draggable from 'vuedraggable'
-	import { storeToRefs } from 'pinia'
-	import { PencilSquareIcon, PlusCircleIcon } from '@heroicons/vue/24/solid'
+import { defineComponent, ref, onMounted, watch, computed } from 'vue'
+import Draggable from 'vuedraggable'
+import { storeToRefs } from 'pinia'
+import { PencilSquareIcon, PlusCircleIcon } from '@heroicons/vue/24/solid'
 
-	import GroupCard from '@/components/Sidebar/GroupCard.vue'
+import GroupCard from '@/components/Sidebar/GroupCard.vue'
+import Dropdown from '@/components/Dropdown.vue'
 
-	import { useProjectStore, useNavigationStore } from '@/stores/main'
-	import { FilterRegistry } from '@/models/filters'
-	import { createStandardFilters, calculateGroups } from '@/utils/projectUtils'
+import { useProjectStore, useNavigationStore } from '@/stores/main'
+import { useSpeckleStore } from '@/stores/speckle'
+import { useFirebaseStore } from '@/stores/firebase'
+import { createStandardFilters, calculateGroups } from '@/utils/projectUtils'
+import { updateMapping } from '@/utils/material'
 
-	import type { NestedGroup } from '@/models/filters'
-	import type { Filter, Group, FilterList } from '@/models/filters'
-	import { useSpeckleStore } from '@/stores/speckle'
+import { FilterRegistry } from '@/models/filters'
+import type { NestedGroup, FilterList } from '@/models/filters'
+import type { dropdownItem } from '../Dropdown.vue'
+import type { Mapping } from '@/models/material'
 
-	export default defineComponent({
-		name: 'GroupList',
-		components: {
-			PencilSquareIcon,
-			PlusCircleIcon,
-			Draggable,
-			GroupCard
-		},
-		setup() {
-			const projectStore = useProjectStore()
-			const navStore = useNavigationStore()
-			const speckleStore = useSpeckleStore()
+const useFetchDropdownItems = (navStore, firebaseStore, speckleStore) => {
+	const dropdownItems = ref<dropdownItem[]>([])
 
-			const currSlideName = computed(() => {
-				if (navStore.activePage === 'Overview') return 'Edit filters'
-				else if (navStore.activePage === 'Mapping') return 'Edit materials'
-				else if (navStore.activePage === 'Results') return 'Edit results'
-				else if (navStore.activePage === 'Benchmark') return 'Edit results'
-				else return null
-			})
+	const fetchDropdownItems = async () => {
+		switch (navStore.activePage) {
+			case 'Overview': {
+				const projectFilters = await firebaseStore.fetchLatestFilters(speckleStore.selectedProject.id)
+				const genericFilter = await firebaseStore.fetchGenericFilters()
 
-			const { filterRegistry, projectGroups } = storeToRefs(projectStore)
-			const refTree = ref<NestedGroup[]>([])
-
-			const toggleSlideover = () => {
-				navStore.toggleSlideover()
+				dropdownItems.value = [
+					...projectFilters.map(log => ({ 
+						name: log.stackName,
+						data: JSON.stringify(log.filterCallStack)
+					})),
+					...genericFilter.map(log => ({ 
+						name: log.stackName,
+						data: JSON.stringify(log.filterCallStack)
+					}))
+				]
+				break
 			}
+			case 'Mapping': {
+				const projectFilters = await firebaseStore.fetchMappings(speckleStore.selectedProject.id)
 
-			const addGroup = () => {
-				navStore.toggleGroupModal()
+				dropdownItems.value = [
+					...projectFilters.map(log => ({ 
+						name: log.mapping.name,
+						data: JSON.stringify(log.mapping)
+					}))
+				]
+				break
 			}
+			case 'Results':
+			case 'Benchmark': {
+				const projectFilters = await firebaseStore.fetchResults(speckleStore.selectedProject.id)
 
-			const toggleColorMode = () => {
-				navStore.toggleColorMode()
+				dropdownItems.value = [
+					...projectFilters.map(log => ({ 
+						name: log.results.date.toDateString(),
+						data: JSON.stringify(log.results)
+					}))
+				]
+				break
 			}
-
-			onMounted(() => {
-				setStandardFilters()
-				calculateGroups(true)
-				refTree.value =	projectStore.getGroupTree()?.children
-				speckleStore.calculateGroupColors(refTree.value)
-			})
-
-			/**
-			 * Remake the tree if change is done on filterss
-			 */
-			watch(
-				() => filterRegistry.value?.filterCallStack,
-				() => {
-					calculateGroups(true)
-					refTree.value =	projectStore.getGroupTree()?.children
-					speckleStore.calculateGroupColors(refTree.value)
-					speckleStore.hideUnusedObjects(speckleStore.hiddenObjects.map(obj => obj.id))
-				}
-			)
-
-			/**
-			 * Remake the tree if change is done on the group list
-			 */
-			watch(
-				projectGroups,
-				() => {
-					calculateGroups(false)
-				},
-				{ deep: true }
-			)
-
-			/**
-			 * Set the standard filters for the project, this is mostly for testing
-			 * should be a popup or something better later on
-			 */
-			const setStandardFilters = () => {
-				//Create new filterregistry, maybe store this in the projectStore?
-				const exampleRegistry = new FilterRegistry()
-				createStandardFilters(exampleRegistry)
-
-				//We use filterlists to create the tree
-				const testFilters: FilterList = {
-					name: 'testFiltering',
-					callStack: [
-						{
-							name: 'groupBy',
-							field: 'speckle_type'
-						}
-					]
-				}
-
-				exampleRegistry.filterCallStack = testFilters
-				projectStore.setFilterRegistry(exampleRegistry)
-			}
-
-			return {
-				refTree,
-				currSlideName,
-				toggleSlideover,
-				addGroup,
-				toggleColorMode
-			}
+			default:
+				return null
 		}
-	})
+	}
+
+	return {
+		dropdownItems,
+		fetchDropdownItems
+	}
+}
+
+const useHandleSelected = (navStore, projectStore) => {
+	const handleSelected = (item: any) => {
+		try {
+			switch (navStore.activePage) {
+				case 'Overview': {
+					const filterList = JSON.parse(item) as FilterList
+					projectStore.updateRegistryStack(filterList.name, filterList.callStack)
+					break
+				} 
+				case 'Mapping': {
+					const mapping = JSON.parse(item) as Mapping
+					updateMapping(mapping)
+					break
+				}
+				case 'Results':
+				case 'Benchmark':
+					//projectStore.updateResultState(item)
+					break
+				default:
+					throw new Error(`Unsupported active page: ${navStore.activePage}`)
+			}
+		} catch (error) {
+			console.error(error)
+		}
+	}
+
+	return {
+		handleSelected
+	}
+}
+
+export default defineComponent({
+	name: 'GroupList',
+	components: {
+		PencilSquareIcon,
+		PlusCircleIcon,
+		Draggable,
+		GroupCard,
+		Dropdown
+	},
+	setup() {
+		const projectStore = useProjectStore()
+		const navStore = useNavigationStore()
+		const speckleStore = useSpeckleStore()
+		const firebaseStore = useFirebaseStore()
+
+		const currSlideName = computed(() => {
+			if (navStore.activePage === 'Overview') return 'Edit filters'
+			else if (navStore.activePage === 'Mapping') return 'Edit materials'
+			else if (navStore.activePage === 'Results') return 'Edit results'
+			else if (navStore.activePage === 'Benchmark') return 'Edit results'
+			else return null
+		})
+
+		const { dropdownItems, fetchDropdownItems } = useFetchDropdownItems(navStore, firebaseStore, speckleStore)
+		const { handleSelected } = useHandleSelected(navStore, projectStore)
+
+		const { filterRegistry, projectGroups } = storeToRefs(projectStore)
+		const refTree = ref<NestedGroup[]>([])
+
+		const toggleSlideover = () => {
+			navStore.toggleSlideover()
+		}
+
+		const addGroup = () => {
+			navStore.toggleGroupModal()
+		}
+
+		const toggleColorMode = () => {
+			navStore.toggleColorMode()
+		}
+
+		/**
+		 * Set the standard filters for the project, this is mostly for testing
+		 * should be a popup or something better later on
+		 */
+		 const setStandardFilters = () => {
+			//Create new filterregistry, maybe store this in the projectStore?
+			const exampleRegistry = new FilterRegistry()
+			createStandardFilters(exampleRegistry)
+
+			//We use filterlists to create the tree
+			const testFilters: FilterList = {
+				name: 'testFiltering',
+				callStack: [
+					{
+						name: 'groupBy',
+						field: 'speckle_type'
+					}
+				]
+			}
+
+			exampleRegistry.filterCallStack = testFilters
+			projectStore.setFilterRegistry(exampleRegistry)
+		}
+
+		onMounted(() => {
+			setStandardFilters()
+			calculateGroups(true)
+			refTree.value = projectStore.getGroupTree()?.children
+			speckleStore.calculateGroupColors(refTree.value)
+		})
+
+		watch(
+			() => navStore.activePage,
+			() => {
+				fetchDropdownItems()
+			},
+			{ immediate: true }
+		)
+
+		watch(
+			() => filterRegistry.value?.filterCallStack,
+			() => {
+				calculateGroups(true)
+				refTree.value = projectStore.getGroupTree()?.children
+				speckleStore.calculateGroupColors(refTree.value)
+				speckleStore.hideUnusedObjects(speckleStore.hiddenObjects.map(obj => obj.id))
+			}
+		)
+
+		watch(
+			projectGroups,
+			() => {
+				calculateGroups(false)
+			},
+			{ deep: true }
+		)
+
+		return {
+			refTree,
+			currSlideName,
+			dropdownItems,
+			handleSelected,
+			toggleSlideover,
+			addGroup,
+			toggleColorMode
+		}
+	}
+})
 </script>
