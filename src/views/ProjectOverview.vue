95b7c22b1b26b3fccb06362e47fce8f3cdd56efa
fabianlinkflink <fabian.sellberg@linkarkitektur.se>
2025-02-27 08:49:51 +0100

    Buildable

--- src/views/ProjectOverview.vue
+++ src/views/ProjectOverview.vue
@@ -43,9 +43,7 @@
 
       <button
         type="button"
-        class="inline-flex w-full justify-center rounded-md bg-green-600 px-3 py-3 my-3 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
-        @click="loadProject"
-      >
+        class="inline-flex w-full justify-center rounded-md bg-green-600 px-3 py-3 my-3 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"      >
         Load this version
       </button>
 
