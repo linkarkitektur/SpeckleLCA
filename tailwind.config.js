5e51a3ae5f867d115dd12bc8d53271b87773affa
fabianlinkflink <fabian.sellberg@linkarkitektur.se>
2025-02-28 08:33:39 +0100

    Styling classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,28 @@ module.exports = {
         bread: ['Merriweather', 'serif'],
       },
     },
+    patterns: {
+      opacities: {
+          100: "1",
+          80: ".80",
+          60: ".60",
+          40: ".40",
+          20: ".20",
+          10: ".10",
+          5: ".05",
+      },
+      sizes: {
+          1: "0.25rem",
+          2: "0.5rem",
+          4: "1rem",
+          6: "1.5rem",
+          8: "2rem",
+          16: "4rem",
+          20: "5rem",
+          24: "6rem",
+          32: "8rem",
+      }
+    },
   },
   variants: {
     extend: {},
@@ -25,6 +47,7 @@ module.exports = {
   blocklist: [], // ['container', 'collapse']
   plugins: [
     require('@tailwindcss/forms'),
+    require('tailwindcss-bg-patterns'),
     // require('@tailwindcss/aspect-ratio'),
   ],
 }
