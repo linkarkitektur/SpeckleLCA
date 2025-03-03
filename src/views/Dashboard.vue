65415a0a01d1c36549b047cbe14d316d9a98f277
fabianlinkflink <fabian.sellberg@linkarkitektur.se>
2025-03-03 22:06:04 +0100

    simplified Views

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -7,6 +7,14 @@
 	<AssemblyModal />
 	<SettingsModal />
 
+	<div 
+    class="fixed inset-0 w-full h-full pattern-dots pattern-black pattern-bg-transparent pattern-size-4 -z-20"
+    :style="{
+      backgroundColor: navStore.activeColor,
+      '--pattern-opacity': '0.2'
+    }"
+  ></div>
+
   <!-- App area -->
 	<div class="flex">
 		<Navbar />
--- src/views/ProjectOverview.vue
+++ src/views/ProjectOverview.vue
@@ -8,7 +8,7 @@
   <div 
     class="fixed inset-0 w-full h-full pattern-dots pattern-black pattern-bg-transparent pattern-size-4"
     :style="{backgroundColor: navStore.activeColor}"
-    style="--pattern-opacity: 0.5;"
+    style="--pattern-opacity: 0.2;"
   ></div>
   
   <!-- Main content -->
