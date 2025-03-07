fb44162f83507be4c70d3cfbd0fde7193203284e
fabianlinkflink <fabian.sellberg@linkarkitektur.se>
2025-03-07 11:23:00 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,33 @@
 <template>
-	<div id="app">
-		<pacman-loader/>
-		<router-view />
-	</div>
+  <div id="app">
+    <PacmanLoader />
+    <router-view />
+  </div>
 </template>
 
-<script lang="ts">
-	import { defineComponent } from 'vue'
-	import { useRoute } from 'vue-router'
-	import { logMessageToSentry } from './utils/monitoring'
-	import pacmanLoader from '@/components/Misc/PacmanLoader.vue'
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+import { logMessageToSentry } from './utils/monitoring'
+import PacmanLoader from '@/components/Misc/PacmanLoader.vue'
 
-	/**
-	 * The main application component.
-	 */
-	export default defineComponent({
-		name: 'SpeckLCA',
-		components: {
-			pacmanLoader,
-		},
-		setup() {
-			const route = useRoute()
+const route = useRoute()
 
-			/**
-			 * Checks if the route has been redirected from another route.
-			 *
-			 * @param {Object} route - The route object.
-			 * @returns {boolean} - Returns true if the route has been redirected, otherwise false.
-			 */
-			if (route.redirectedFrom !== null) {
-				const originalPath = route.redirectedFrom
-				const redirectPath = route.path
-				const query = route.query
-				const params = route.params
+// Check for route redirects on mount
+onMounted(() => {
+  if (route.redirectedFrom) {
+    const originalPath = route.redirectedFrom
+    const redirectPath = route.path
+    const query = route.query
+    const params = route.params
 
-				// Log the redirect route to Sentry as a warning message.
-				logMessageToSentry(
-					`Route redirected from ${originalPath} to ${redirectPath} with query params ${JSON.stringify(
-						query
-					)} and params ${JSON.stringify(params)}`,
-					'warning'
-				)
-			}
-			return {
-				route
-			}
-		}
-	})
+    // Log the redirect route to Sentry as a warning message
+    logMessageToSentry(
+      `Route redirected from ${originalPath} to ${redirectPath} with query params ${JSON.stringify(
+        query
+      )} and params ${JSON.stringify(params)}`,
+      'warning'
+    )
+  }
+})
 </script>
