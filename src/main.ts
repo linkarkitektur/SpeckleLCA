6ed93f36aa6d0c3bec7774b9dc1c08c91fd1fc86
fabianlinkflink <fabian.sellberg@linkarkitektur.se>
2025-04-09 06:54:12 +0200

    Added google analytics

--- src/main.ts
+++ src/main.ts
@@ -6,64 +6,30 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import { createGtag } from 'vue-gtag'
 
 import App from './App.vue'
 import router from './router'
-import * as Sentry from '@sentry/vue'
 import { initializeFirebase } from '@/firebase'
 
 import './index.css'
-import type { Vue } from '@sentry/vue/types/types'
 
 import { clickOutsideDirective } from '@/directives/clickDirectives'
 
 // Create the Vue 3 application.
 const app = createApp(App)
 
-// Initialize Sentry for application-wide
-// error monitoring and performance tracking.
-Sentry.init({
-  app,
-  // dsn: import.meta.env.VITE_SENTRY_DSN,
-  debug: false, // TODO Set this to `false` for production
-  attachStacktrace: true,
-  integrations: [
-    new Sentry.BrowserTracing({
-      // TODO Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-      tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
-      routingInstrumentation: Sentry.vueRouterInstrumentation(router)
-    }),
-    new Sentry.Replay({
-      // Additional SDK configuration:
-      maskAllText: true,
-      blockAllMedia: true
-    })
-  ],
-  // Performance (set appropriately for production.)
-  tracesSampleRate: 0.1, // Capture 100% of transactions
-  replaysSessionSampleRate: 0.5, // Change the sample rate to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0 // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-})
-
-/**
- * Configures global error handling for Vue application.
- * @param {Vue} app - The Vue 3 application instance.
- */
-export function configureGlobalErrorHandling(app: Vue) {
-  app.config.errorHandler = (err: any, vm: any, info: any) => {
-    Sentry.captureException(err, {
-      extra: { info }
-    })
-    console.error(`Error: ${err}, Info: ${info}`)
-  }
-}
-
 // Setup our Pinia store, and initialize the router.
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
 
+const gtag = createGtag({
+  tagId: "G-M3Y1M4BRW5"
+})
+
 app.use(pinia)
 app.use(router)
+app.use(gtag)
 
 // Create directives
 app.directive('click-outside' ,clickOutsideDirective)
